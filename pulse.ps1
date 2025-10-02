6a53e04aaca56b1cc65c5be67ff6ad8e08afaee7
Ihor Meneg <4191524+ex1234@users.noreply.github.com>
2025-10-02 16:40:42 +0200

    feat: add UTF-8 content & CI

--- pulse.ps1
+++ pulse.ps1
@@ -1,6 +1,6 @@
-﻿param([string]\ = (Get-Date -Format 'yyyy-MM-dd HH:mm:ss'))
-if (-not (Test-Path '.pulse')) { New-Item -ItemType File -Path '.pulse' | Out-Null }
-\ | Out-File -FilePath '.pulse' -Append
+﻿param([string]$Note = (Get-Date -Format "yyyy-MM-dd HH:mm:ss"))
+if (-not (Test-Path ".pulse")) { New-Item -ItemType File -Path ".pulse" | Out-Null }
+$Note | Out-File -FilePath ".pulse" -Append -Encoding utf8
 git add .pulse
-git commit -m "chore(pulse): \"
+git commit -m "chore(pulse): $Note"
 git push
